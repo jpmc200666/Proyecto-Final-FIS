d7b32a38cd518d1a48d9d86431af4b5ea2c3f740
JonaCaste <jonacasteb@gmail.com>
2025-07-02 06:34:02 -0500

    add validation to inactive carrito of cliente

--- proyectoFinal/src/main/java/com/ingenieriasoftware/proyectoFinal/repositories/ClienteRepository.java
+++ proyectoFinal/src/main/java/com/ingenieriasoftware/proyectoFinal/repositories/ClienteRepository.java
@@ -2,11 +2,20 @@ package com.ingenieriasoftware.proyectoFinal.repositories;
 
 
 import com.ingenieriasoftware.proyectoFinal.models.Cliente;
+import com.ingenieriasoftware.proyectoFinal.models.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // Repositorio para gestionar operaciones CRUD en la entidad { Cliente }.
 
 @Repository
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+//    Optional<Cliente> findById(@Param("id") Long id);
+
 }
--- proyectoFinal/src/main/java/com/ingenieriasoftware/proyectoFinal/service/imp/CarritoServiceImpl.java
+++ proyectoFinal/src/main/java/com/ingenieriasoftware/proyectoFinal/service/imp/CarritoServiceImpl.java
@@ -1,13 +1,18 @@
 package com.ingenieriasoftware.proyectoFinal.service.imp;
 
 import com.ingenieriasoftware.proyectoFinal.models.Carrito;
+import com.ingenieriasoftware.proyectoFinal.models.Cliente;
 import com.ingenieriasoftware.proyectoFinal.models.Pedido;
 import com.ingenieriasoftware.proyectoFinal.repositories.CarritoRepository;
+import com.ingenieriasoftware.proyectoFinal.repositories.ClienteRepository;
 import com.ingenieriasoftware.proyectoFinal.repositories.PedidoRepository;
 import com.ingenieriasoftware.proyectoFinal.service.interfaces.ICarritoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -19,11 +24,59 @@ public class CarritoServiceImpl implements ICarritoService {
     @Autowired
     private PedidoRepository pedidoRepository;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    /**
+     * Retornamos el carrito activo (si existe) para el cliente seleccionadado
+     * Si el pedido no existe, creamos un nuevo pedido
+     * @param clientId  id del cliente
+     * @return Carrito
+     */
     @Override
     public Carrito getActiveByUser(Long clientId) {
 
+        /**
+         * Debe existir un carrito por pedido
+         * La relacion con cliente se realiza en pedido
+         * Buscamos el pedido activo del cliente enviado (solo debe existir un pedido activo)
+         */
         Optional<Pedido> pedidoOptional = pedidoRepository.findByClienteAndEstado(clientId, "AC");
         if (pedidoOptional.isPresent()) return pedidoOptional.get().getCarrito();
+
+        /**
+         * Si no existe, creamos un pedido y un carrito nuevo
+         */
+        else{
+
+            Optional<Cliente> clienteOpt = clienteRepository.findById(clientId);
+
+            if(clienteOpt.isPresent()){
+                Carrito nuevoCarrito = Carrito.builder()
+                        .fechaCreacion(new Date())
+                        .vigencia(300)
+                        .totalCarrito(BigDecimal.ZERO)
+                        .build();
+
+                carritoRepository.save(nuevoCarrito);
+
+                Pedido nuevoPedido = Pedido.builder()
+                        .fecha(LocalDate.now())
+                        .estado("AC")
+                        .carrito(nuevoCarrito)
+//                    .pago(pago)
+                        .cliente(clienteOpt.get())
+                        .build();
+
+                pedidoRepository.save(nuevoPedido);
+
+                return nuevoCarrito;
+            }
+            /**
+             * @TODO Crear el manejador de error para esto
+             */
+            else System.out.println("El cliente no existe");
+        }
 
 //        carritoRepository.
         return null;
